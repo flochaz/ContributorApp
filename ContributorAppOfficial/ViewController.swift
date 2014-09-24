29aadb6a7e786111eab8d6cc6614391dbe4b00e3
florian chazal <florianchazal@gmail.com>
2014-09-24 07:41:13 -0700

    Initial Commit

---
+++ ContributorAppOfficial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContributorAppOfficial
+//
+//  Created by florian chazal on 9/24/14.
+//  Copyright (c) 2014 florian chazal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
